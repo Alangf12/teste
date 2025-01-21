d85d85d2ffe28bf174f2d74dd6d5d11cf5dc1c89
Alangf12 <140029358+Alangf12@users.noreply.github.com>
2025-01-20 23:09:25 -0300

    Add files via upload

---
+++ generate.js
@@ -0,0 +1,29 @@
+{
+  "videos": [
+    {
+      "id": 1,
+      "title": "Vídeo 1",
+      "description": "Descrição do vídeo 1",
+      "url": "https://g2v8.sosacdn.com/remote_control.php?time=1737422522&cv=7988d959acaedafa9e5d0e10cfd96cf5&lr=0&cv2=190ce1ed937e38b06a01d73502cb6d48&file=%2Fcontents%2Fvideos%2F55000%2F55332%2F55332.mp4&cv3=00e8f2adb33db2a93d2c1316ab0e3740&cv4=15d723d5c32759b063037218fff08c00",
+      "thumbnail": "https://tvoi-kumiry.ru/wp-content/uploads/2023/07/273530701_438906931253013_3459942637435602752_n.jpg",
+      "duration": "5:23"
+    },
+    {
+      "id": 2,
+      "title": "Vídeo 2",
+      "description": "Descrição do vídeo 2",
+      "url": "https://g2v8.sosacdn.com/remote_control.php?time=1737422522&cv=7988d959acaedafa9e5d0e10cfd96cf5&lr=0&cv2=190ce1ed937e38b06a01d73502cb6d48&file=%2Fcontents%2Fvideos%2F55000%2F55332%2F55332.mp4&cv3=00e8f2adb33db2a93d2c1316ab0e3740&cv4=15d723d5c32759b063037218fff08c00",
+      "thumbnail": "https://tvoi-kumiry.ru/wp-content/uploads/2023/07/273530701_438906931253013_3459942637435602752_n.jpg",
+      "duration": "7:10"
+    },
+    {
+      "id": 3,
+      "title": "Vídeo 3",
+      "description": "Descrição do vídeo 3",
+      "url": "https://g2v8.sosacdn.com/remote_control.php?time=1737422522&cv=7988d959acaedafa9e5d0e10cfd96cf5&lr=0&cv2=190ce1ed937e38b06a01d73502cb6d48&file=%2Fcontents%2Fvideos%2F55000%2F55332%2F55332.mp4&cv3=00e8f2adb33db2a93d2c1316ab0e3740&cv4=15d723d5c32759b063037218fff08c00",
+      "thumbnail": "https://tvoi-kumiry.ru/wp-content/uploads/2023/07/273530701_438906931253013_3459942637435602752_n.jpg",
+      "duration": "8:45"
+    },
+    ...
+  ]
+}
